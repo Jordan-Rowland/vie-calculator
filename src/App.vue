1f08eab12f1f7bee74b86daed032f7e2922e78bc
Jordan <jordanrowland00@gmail.com>
2019-06-09 16:16:07 -0700

    finished v1 calculator

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,178 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+    <div class="result-display">
+      {{ number1 }}
+      {{ calculationType }}
+      {{ number2 }}
+    </div>
+    <br>
+    <div class="columns">
+      <div class="column col-3">
+        <div class="app-button">
+        <button @click="add">+</button>
+        </div>
+      </div>
+      <div class="column col-3">
+        <div class="app-button">
+        <button @click="subtract">-</button>
+        </div>
+      </div>
+      <div class="column col-3">
+        <div class="app-button">
+        <button @click="multiply">*</button>
+        </div>
+      </div>
+      <div class="column col-3">
+        <div class="app-button">
+        <button @click="divide">/</button>
+        </div>
+      </div>
+    <div class="column col-3"
+    v-for="number in buttonNumbers">
+    <div class="app-button">
+      <app-button
+      :number="number"
+      @parentClicked="enterNumber"
+      ></app-button>
+    </div>
+    </div>
+    <div class="column col-3">
+    <div class="app-button">
+    <button @click="calculation">=</button>
+    </div>
+    </div>
+    <div class="column col-3">
+      <div class="app-button">
+      <button @click="clear">C</button>
+      </div>
+    </div>
+    </div>
+    </div>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import Vue from 'vue';
+import Button from './components/Button.vue';
+import Display from './components/Display.vue';
 
-export default {
+export default Vue.extend({
   name: 'app',
   components: {
-    HelloWorld
+    'appButton': Button,
+    'appDisplay': Display
+  },
+  data() {
+    return {
+      number1: '',
+      number2: '',
+      buttonNumbers: [1,2,3,4,5,6,7,8,9,0],
+      calculationType: ''
+    }
+  },
+  methods: {
+    calculation() {
+      if (this.calculationType === '+') {
+        this.number1 = Number(this.number1) + Number(this.number2)
+      }
+      else if (this.calculationType === '-') {
+        this.number1 = Number(this.number1) - Number(this.number2)
+      }
+      else if (this.calculationType === '*') {
+        this.number1 = Number(this.number1) * Number(this.number2)
+      }
+      else if (this.calculationType === '/') {
+        this.number1 = Number(this.number1) / Number(this.number2)
+      }
+      this.number2 = ''
+      this.calculationType = ''
+    },
+    add() {
+      if (this.number2) {
+        this.calculation()
+      }
+      this.calculationType = '+'
+    },
+    subtract() {
+      if (this.number2) {
+        this.calculation()
+      }
+      this.calculationType = '-'
+    },
+    multiply() {
+      if (this.number2) {
+        this.calculation()
+      }
+      this.calculationType = '*'
+    },
+    divide() {
+      if (this.number2) {
+        this.calculation()
+      }
+      this.calculationType = '/'
+    },
+    enterNumber(event) {
+      if (!this.calculationType) {
+        this.number1 += String(event)
+      }
+      else {
+        this.number2 += String(event)
+      }
+    },
+    clear() {
+      this.number1 = ''
+      this.number2 = ''
+      this.calculationType = ''
+    }
   }
-}
+});
 </script>
 
-<style>
+<style scoped>
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin: auto;
   margin-top: 60px;
+  width: 400px;
+  border: 2px solid black;
+  border-radius: 7px;
+  padding: 15px;
+  background-color: #ddf;
 }
+
+.app-button {
+  border: 1px solid black;
+  border-radius: 10px;
+  width: 65px;
+  height: 65px;
+  margin: auto;
+  background-color: #def;
+  margin-top: 13px;
+}
+
+button {
+  background-color: #def;
+  border: none;
+  margin-top: 20px;
+}
+
+.app-button:hover, button:hover {
+  cursor: pointer;
+}
+
+.result-display {
+  background-color: #adf;
+  height: 50px;
+  border: 1px solid #bcb;
+  border-radius: 8px;
+  margin: auto;
+  padding-top: 13px;
+}
+
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,35 @@
+<template>
+  <div @click="clicked">
+    <p>
+    {{ number }}
+    </p>
+  </div>
+</template>
+
+<script>
+/* jshint esversion: 9 */
+export default {
+  name: 'appbutton',
+  props: ["number"],
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+    clicked() {
+      this.$emit('parentClicked', this.number)
+    }
+  }
+};
+</script>
+
+<style scoped>
+div:hover {
+  cursor: pointer;
+}
+
+p {
+  margin-top: 20px;
+}
+</style>
